7bee2ccbc65ba20cb63cca47f484a904175bd4ac
Ali Uluğ <keremaliulug@gmail.com>
2016-07-22 10:08:51 +0300

    Add project files.

---
+++ SirketPowerBIGuncelleyen/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace SirketPowerBIGuncelleyen
+{
+    partial class AnaForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtGuncelenecekDosyaAdi = new System.Windows.Forms.TextBox();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnDosyaSec = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtGuncelenecekDosyaAdi
+            // 
+            this.txtGuncelenecekDosyaAdi.Location = new System.Drawing.Point(94, 2);
+            this.txtGuncelenecekDosyaAdi.Multiline = true;
+            this.txtGuncelenecekDosyaAdi.Name = "txtGuncelenecekDosyaAdi";
+            this.txtGuncelenecekDosyaAdi.ReadOnly = true;
+            this.txtGuncelenecekDosyaAdi.Size = new System.Drawing.Size(572, 66);
+            this.txtGuncelenecekDosyaAdi.TabIndex = 0;
+            this.txtGuncelenecekDosyaAdi.TabStop = false;
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(673, 2);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(90, 66);
+            this.btnGuncelle.TabIndex = 1;
+            this.btnGuncelle.Text = "Seçili Dosyayı Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnDosyaSec
+            // 
+            this.btnDosyaSec.Location = new System.Drawing.Point(1, 2);
+            this.btnDosyaSec.Name = "btnDosyaSec";
+            this.btnDosyaSec.Size = new System.Drawing.Size(87, 66);
+            this.btnDosyaSec.TabIndex = 1;
+            this.btnDosyaSec.Text = "Güncellenecek Dosya Seç";
+            this.btnDosyaSec.UseVisualStyleBackColor = true;
+            this.btnDosyaSec.Click += new System.EventHandler(this.btnDosyaSec_Click);
+            // 
+            // AnaForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(766, 71);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.txtGuncelenecekDosyaAdi);
+            this.Controls.Add(this.btnDosyaSec);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "AnaForm";
+            this.Text = "Power BI Güncelleyen";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.AnaForm_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtGuncelenecekDosyaAdi;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnDosyaSec;
+    }
+}
+
---
+++ SirketPowerBIGuncelleyen/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+using System.IO.Compression;
+
+namespace SirketPowerBIGuncelleyen
+{
+    public partial class AnaForm : Form
+    {
+        private const string IndirilenDosya = "guncel.zip";
+        private const string GeciciDosya = "geciciDosya";
+        private bool _guncelDosyaIndirildi = false;
+        private readonly string _randomKlasor;
+
+        public AnaForm()
+        {
+            InitializeComponent();
+
+            _randomKlasor = Guid.NewGuid().ToString();
+            DirectoryInfo directory = Directory.CreateDirectory(_randomKlasor);
+            //directory.Attributes |= FileAttributes.Hidden;
+            Directory.SetCurrentDirectory(_randomKlasor);
+        }
+
+        private void btnDosyaSec_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = @"Power BI Files (.pbix)|*.pbix";
+            if (fd.ShowDialog()==DialogResult.OK)
+            {
+                txtGuncelenecekDosyaAdi.Text = fd.FileName;
+            }
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            string dosyaAdi = txtGuncelenecekDosyaAdi.Text;
+            if (!guncellenecekDosyaUygun(dosyaAdi))
+                return;
+
+            if (!_guncelDosyaIndirildi)
+            {
+                guncelDosyayiIndir();
+                _guncelDosyaIndirildi = true;
+            }
+
+            FileInfo fi = new FileInfo(dosyaAdi);
+            string yeniDosyaAdi = fi.Name.Substring(0, fi.Name.Length - 5) + ".zip";
+            File.Copy(dosyaAdi, yeniDosyaAdi, true);
+
+            zipiAktar(IndirilenDosya, yeniDosyaAdi);
+
+            string guid = Guid.NewGuid().ToString().Substring(0, 7);
+            string yeniPbixDosya = fi.FullName.Substring(0, fi.FullName.Length - 5) + "_" + guid + ".pbix";
+            File.Move(yeniDosyaAdi, yeniPbixDosya);
+
+            MessageBox.Show("Seçili dosyanın veri modeli güncellendi. Yeni dosya yolu:\n" + yeniPbixDosya);
+        }
+
+        private bool guncellenecekDosyaUygun(string dosyaPath)
+        {
+            if (string.IsNullOrWhiteSpace(dosyaPath))
+            {
+                MessageBox.Show(@"Güncellenecek dosya seçmelisiniz.");
+                return false;
+            }
+
+            if (!File.Exists(dosyaPath))
+            {
+                MessageBox.Show(@"Güncellenecek dosya bulunamadı.");
+                return false;
+            }
+
+            return true;
+        }
+
+        private void zipiAktar(string guncelDosya, string guncellenecekDosya)
+        {
+            using (FileStream guncel = new FileStream(guncelDosya, FileMode.Open))
+            using (FileStream guncellenecek = new FileStream(guncellenecekDosya, FileMode.Open))
+            using (ZipArchive archiveGuncel = new ZipArchive(guncel, ZipArchiveMode.Read))
+            using (ZipArchive archiveGuncellenecek = new ZipArchive(guncellenecek, ZipArchiveMode.Update))
+            {
+                dosyayiBirZiptenDigerineAktar(archiveGuncel, archiveGuncellenecek, "DataModel");
+                dosyayiBirZiptenDigerineAktar(archiveGuncel, archiveGuncellenecek, "DataMashup");
+                dosyayiBirZiptenDigerineAktar(archiveGuncel, archiveGuncellenecek, "Metadata");
+                dosyayiBirZiptenDigerineAktar(archiveGuncel, archiveGuncellenecek, "DiagramState");
+            }
+        }
+
+        private void dosyayiBirZiptenDigerineAktar(ZipArchive kaynakArsiv, ZipArchive hedefArsiv, string dosyaAdi)
+        {
+            ZipArchiveEntry entry = kaynakArsiv.GetEntry(dosyaAdi);
+            entry.ExtractToFile(GeciciDosya, true);
+            hedefArsiv.GetEntry(dosyaAdi).Delete();
+            hedefArsiv.CreateEntryFromFile(GeciciDosya, dosyaAdi, CompressionLevel.NoCompression);
+        }
+
+        private void guncelDosyayiIndir()
+        {
+            WebClient client = new WebClient();
+            client.DownloadFile("https://github.com/AdaYazilim/Ada-Sirket-BI-Yardim/blob/master/%C5%9Eablonlar/U-UD-H.pbix?raw=true", IndirilenDosya);
+        }
+
+        private void AnaForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Directory.SetCurrentDirectory(Application.StartupPath);
+            if (Directory.Exists(_randomKlasor))
+                Directory.Delete(_randomKlasor, true);
+        }
+    }
+}
