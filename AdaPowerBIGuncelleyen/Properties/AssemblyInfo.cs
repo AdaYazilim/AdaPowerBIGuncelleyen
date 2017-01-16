7a54a62414e55d3e0729274d036323eac236a556
Serdar <serdar.suvarierol@gmail.com>
2017-01-16 11:49:37 +0300

    SirketPowerBIGuncelleyen isminin AdaPowerBIGuncelleyen'e geçirilmesi eksik olmuş. Kalanlar da düzeltildi.

--- AdaPowerBIGuncelleyen/Properties/AssemblyInfo.cs
+++ AdaPowerBIGuncelleyen/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SirketPowerBIGuncelleyen")]
+[assembly: AssemblyTitle("AdaPowerBIGuncelleyen")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SirketPowerBIGuncelleyen")]
+[assembly: AssemblyProduct("AdaPowerBIGuncelleyen")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
