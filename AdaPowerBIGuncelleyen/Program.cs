6a7712d55402efac12ffe9e0e590587a75c016a9
Serdar <serdar.suvarierol@gmail.com>
2017-07-05 09:12:45 +0300

    test

--- AdaPowerBIGuncelleyen/Program.cs
+++ AdaPowerBIGuncelleyen/Program.cs
@@ -11,6 +11,7 @@ namespace AdaPowerBIGuncelleyen
         [STAThread]
         static void Main()
         {
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new AnaForm());
